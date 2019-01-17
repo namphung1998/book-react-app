7c132b5efa3b7539238c27909a9f2437b30874f6
Nam Phung <nphung@macalester.edu>
2019-01-16 22:32:43 -0600

    Updates

---
+++ precache-manifest.7386acbc13eaab96329db9174b89b59c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8c416af58912f7550c25",
+    "url": "/book-react-app/static/js/main.8c416af5.chunk.js"
+  },
+  {
+    "revision": "ce4877f8e471828a099a",
+    "url": "/book-react-app/static/css/1.4dcc2aa1.chunk.css"
+  },
+  {
+    "revision": "ce4877f8e471828a099a",
+    "url": "/book-react-app/static/js/1.ce4877f8.chunk.js"
+  },
+  {
+    "revision": "f5993072d2185720669f",
+    "url": "/book-react-app/static/js/runtime~main.f5993072.js"
+  },
+  {
+    "revision": "f9cdd5726bc4793428c2743a2ec52d9d",
+    "url": "/book-react-app/index.html"
+  }
+];
